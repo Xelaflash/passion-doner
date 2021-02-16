476a930ef62988b1d112a9a54fac579057fe7311
Alexandre Gros <xelaflash@gmail.com>
2021-02-16 11:14:35 -0400

    🚀 add default img on kebab + 🎨 styling kebab_show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def avatar_for(kebab)
+    kebab_pic = kebab
+    if kebab_pic.photo.attached?
+      kebab_default_img = cl_image_tag(kebab.photo.key, width: 250, height: 250, crop: :fill, class: 'kebab-pic', alt: 'Kebab picture - Passion Döner')
+    else
+      kebab_default_img = image_tag('kebab_default.png', class: 'kebab-pic', alt: 'Kebab picture - Passion Döner')
+    end
+    kebab_default_img
+  end
 end
