c4067050b573df99b8ded4edf3ec3a33af583469
Alexandre Gros <xelaflash@gmail.com>
2021-02-25 14:54:58 -0400

    🎨 add small shadow to map

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -90,7 +90,6 @@ const kebabMap = () => {
   const mapElement = document.getElementById('map');
   if (mapElement) {
     const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
-    // don't try to build a map if there's no div#map to inject in
     const markers = JSON.parse(mapElement.dataset.markers);
     map.addMarkers(markers);
 
