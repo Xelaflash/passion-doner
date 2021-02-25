c3d9519171449c0c7163eb9aa35e24e70b8171f3
Alexandre Gros <xelaflash@gmail.com>
2021-02-25 12:06:49 -0400

    🎨 styling dark mode btn

--- app/javascript/components/darkMode.js
+++ app/javascript/components/darkMode.js
@@ -1,9 +1,12 @@
 function initTheme() {
   const darkThemeSelected =
     localStorage.getItem('color-theme') !== null && localStorage.getItem('color-theme') === 'dark';
-  darkThemeSelected
-    ? document.documentElement.setAttribute('color-theme', 'dark')
-    : document.documentElement.setAttribute('color-theme', 'light');
+  // ? I add the below block in a one liner but eslint yells at me for unused exp so not a one liner...
+  if (darkThemeSelected) {
+    document.documentElement.setAttribute('color-theme', 'dark');
+  } else {
+    document.documentElement.setAttribute('color-theme', 'light');
+  }
 }
 
 const toggleColorTheme = e => {
