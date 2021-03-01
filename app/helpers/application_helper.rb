dec3839ac5ebd188ed779b748953586ce265fb87
Alexandre Gros <xelaflash@gmail.com>
2021-03-01 08:33:42 -0400

    🔨 review after lighthouse audit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,9 +3,9 @@ module ApplicationHelper
   def avatar_for(kebab)
     kebab_pic = kebab
     if kebab_pic.photo.attached?
-      kebab_default_img = cl_image_tag(kebab.photo.key, width: 250, height: 250, crop: :fill, class: 'kebab-pic', alt: 'Kebab picture - Passion Döner')
+      kebab_default_img = cl_image_tag(kebab.photo.key, width: 140, crop: :fill, class: 'kebab-pic', alt: 'Kebab picture - Passion Döner')
     else
-      kebab_default_img = image_tag('kebab_default.png', class: 'kebab-pic', alt: 'Kebab picture - Passion Döner')
+      kebab_default_img = image_tag('kebab_default.png', class: 'kebab-pic', alt: 'Kebab picture - Passion Döner', width: 140)
     end
     kebab_default_img
   end
